ac4779b504eab14a28668de584b75f97771c1468
Logesh Kalyanasundaram <logesh@haleytek.com>
2021-10-18 14:59:28 +0200

    try after installing kubectl on schedular

--- lib/kubernetes_utils.py
+++ lib/kubernetes_utils.py
@@ -5,20 +5,17 @@ from kubernetes import client, config
 
 
 def get_available_pvc() -> List[str]:
-    config.load_kube_config(config_file="/opt/bitnami/airflow/.kube/config")
+    kube_config = "/opt/bitnami/airflow/.kube/config"
+    config.load_kube_config(config_file=kube_config)
     kubectl = client.CoreV1Api()
     pvcs = kubectl.list_persistent_volume_claim_for_all_namespaces()
-
     def out(command):
         result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
         return result.stdout
-
-    list_of_available_pvcs: str = []
     for pvc in pvcs.items:
-        ret = out("kubectl describe pvc " + pvc.metadata.name)
+        ret = out("kubectl describe pvc " + pvc.metadata.name + " --kubeconfig=" + kube_config)
         for line in ret.splitlines():
             if line.startswith("Used By:"):
                 if "<none>" in line:
                     return pvc.metadata.name
-
     return None
